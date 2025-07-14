53dd685bb55656a29a2299efdaffe2ef1fb5ea80
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2025-07-14 07:55:18 +0600

    Load purchased items on page load

--- ShopInventory/Views/PurchasedItemsPage.xaml.cs
+++ ShopInventory/Views/PurchasedItemsPage.xaml.cs
@@ -1,9 +1,22 @@
+using ShopInventory.ViewModels;
+
 namespace ShopInventory.Views;
 
 public partial class PurchasedItemsPage : ContentPage
 {
-	public PurchasedItemsPage()
+	public PurchasedItemsPage(PurchasedItemsViewModel viewModel)
 	{
 		InitializeComponent();
+		BindingContext = viewModel;
+	}
+
+	protected override async void OnAppearing()
+	{
+		base.OnAppearing();
+		
+		if (BindingContext is PurchasedItemsViewModel viewModel)
+		{
+			await viewModel.LoadItems();
+		}
 	}
 }
